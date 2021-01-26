8336966bf7f2694ad0cc6b5763671e248103e724
Becky Smith <rebkwok@gmail.com>
2021-01-26 20:28:12 +0000

    more tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,8 +7,8 @@ import pytest
 
 @pytest.fixture
 def temp_db_path():
-    test_db = Path(__file__).parent / 'fixtures/test_db.kdbx'
-    temp_db = Path(__file__).parent / 'fixtures/temp_db.kdbx'
+    test_db = Path(__file__).parent / "fixtures/test_db.kdbx"
+    temp_db = Path(__file__).parent / "fixtures/temp_db.kdbx"
     shutil.copy(test_db, temp_db)
     yield temp_db
     temp_db.unlink()
@@ -17,5 +17,30 @@ def temp_db_path():
 @pytest.fixture
 def test_db_path():
     def named_test_db_path(db_name):
-        return Path(__file__).parent / f'fixtures/{db_name}.kdbx'
+        return Path(__file__).parent / f"fixtures/{db_name}.kdbx"
     yield named_test_db_path
+
+
+@pytest.fixture
+def mock_config_file():
+    # HOME is set to the fixtures file in tests
+    temp_config_path = Path(__file__).parent / "fixtures/.kp"
+    temp_config_path.mkdir(parents=True, exist_ok=True)
+    temp_config_file = temp_config_path / "config.ini"
+    
+    db_path = Path(__file__).parent / f"fixtures/test_db.kdbx"
+    with open(temp_config_file, "w") as outfile:
+        outfile.write(
+            f"""
+            [default]
+            KEEPASSDB={str(db_path)}
+            KEEPASSDB_PASSWORD=badpass
+            
+            [test]
+            KEEPASSDB={str(db_path)}
+            KEEPASSDB_PASSWORD=test
+            """
+        )
+    yield
+    temp_config_file.unlink()
+    temp_config_path.rmdir()
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,6 +3,7 @@ from os import environ
 from pathlib import Path
 from unittest.mock import patch
 
+import pyperclip
 from typer.testing import CliRunner
 
 from kpcli.kp import app
@@ -41,3 +42,83 @@ def test_invalid_credentials_database_with_keyfile():
     result = runner.invoke(app, ["ls"])
     assert result.exit_code == 1
     assert "Invalid credentials" in result.stdout
+
+
+@patch.dict(environ, get_env_vars("test_db"))
+def test_config_file_overrides_env_vars(mock_config_file):
+    # The config file has invalid credentials in the default profile
+    result = runner.invoke(app, ["ls"])
+    assert result.exit_code == 1
+    assert "Invalid credentials" in result.stdout
+
+    # valid credentials in the test profile
+    result = runner.invoke(app, ["--profile", "test", "ls"])
+    assert result.exit_code == 0
+
+
+@patch.dict(environ, get_env_vars("test_db"))
+def test_get():
+    result = runner.invoke(app, ["get", "gmail"])
+    assert result.exit_code == 0
+    assert "MyGroup/gmail" in result.stdout
+
+
+@patch.dict(environ, get_env_vars("test_db"))
+def test_get_with_password():
+    result = runner.invoke(app, ["get", "gmail"])
+    assert "testpass" not in result.stdout
+    assert "********" in result.stdout
+
+    result = runner.invoke(app, ["get", "gmail", "--show-password"])
+    assert "testpass" in result.stdout
+    assert "********" not in result.stdout
+
+
+@patch.dict(environ, get_env_vars("test_db"))
+def test_copy():
+    # copies password by default
+    result = runner.invoke(app, ["cp", "gmail"])
+    assert pyperclip.paste() == "testpass"
+
+    # copy username
+    result = runner.invoke(app, ["cp", "gmail", "username"])
+    assert pyperclip.paste() == "test@test.com"
+
+    # copy username with abbreviation
+    result = runner.invoke(app, ["cp", "gmail", "u"])
+    assert pyperclip.paste() == "test@test.com"
+
+
+@patch.dict(environ, get_env_vars("temp_db"))
+def test_add(temp_db_path):
+    result = runner.invoke(app, ["get", "test entry"])
+    assert "No matching entry found" in result.stdout
+    runner.invoke(
+        app,
+        [
+            "add",
+            "--group", "mygroup",
+            "--title", "a test entry",
+            "--username", "Bugs Bunny",
+            "--password", "carrot"
+        ]
+    )
+    result = runner.invoke(app, ["get", "test entry"])
+    assert "MyGroup/a test entry" in result.stdout
+
+
+@patch.dict(environ, get_env_vars("temp_db"))
+def test_change_password(temp_db_path):
+    result = runner.invoke(app, ["get", "gmail", "--show-password"])
+    assert "testpass" in result.stdout
+    runner.invoke(
+        app,
+        [
+            "change-password",
+            "gmail",
+            "--password", "boop"
+        ]
+    )
+    result = runner.invoke(app, ["get", "gmail", "--show-password"])
+    assert "testpass" not in result.stdout
+    assert "boop" in result.stdout
